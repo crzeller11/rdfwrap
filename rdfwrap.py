3b6d7b0d40b7998fdc1946ae03a33cbb19e1fef7
Chloe <czeller@oxy.edu>
2017-04-18 15:59:32 -0700

    small changes to semantic network

--- rdfwrap.py
+++ rdfwrap.py
@@ -35,29 +35,108 @@ class NXRDF:
 def main():
     # create example graph
     g = NXRDF()
-    mammal = g.add_node()
-    g.add_edge(mammal, 'name', 'mammal')
-    cat = g.add_node()
-    g.add_edge(cat, 'name', 'cat')
-    bear = g.add_node()
-    g.add_edge(bear, 'name', 'bear')
-    fur = g.add_node()
-    g.add_edge(fur, 'name', 'fur')
-    g.add_edge(cat, 'has', fur)
-    g.add_edge(bear, 'has', fur)
-    g.add_edge(cat, 'isa', mammal)
-    g.add_edge(bear, 'isa', mammal)
+
+    bismarck = g.add_node()
+    g.add_edge(bismarck, 'name', 'Bismarck')
+
+    one_ca_nd = g.add_node()
+    g.add_edge(one_ca_nd, 'type', 'predicate')
+    ca = g.add_node()
+    g.add_edge(one_ca_nd, 'source', ca)
+    g.add_edge(ca, 'name', 'California')
+    nd = g.add_node()
+    g.add_edge(bismarck, 'city_state', nd)
+    g.add_edge(nd, 'name', 'North Dakota')
+    g.add_edge(one_ca_nd, 'destination', nd)
+    unknown_dist_1 = g.add_node()
+    g.add_edge(one_ca_nd, 'distance', unknown_dist_1)
+
+    two_nd_wi = g.add_node()
+    g.add_edge(two_nd_wi, 'type', 'predicate')
+    wisco = g.add_node()
+    g.add_edge(wisco, 'name', 'Wisconsin')
+    g.add_edge(two_nd_wi, 'source', wisco)
+    g.add_edge(two_nd_wi, 'destination', nd)
+    unknown_dist_2 = g.add_node()
+    g.add_edge(two_nd_wi, 'distance', unknown_dist_2)
+    g.add_edge(unknown_dist_1, 'greater_than', unknown_dist_2)
+
+    three_city_nd = g.add_node()
+    g.add_edge(three_city_nd, 'type', 'predicate')
+    arb_wi_city = g.add_node()
+    g.add_edge(arb_wi_city, 'name', 'Arbitrary Wisconsin City')
+    g.add_edge(three_city_nd, 'source', arb_wi_city)
+    unknown_dist_3 = g.add_node()
+    g.add_edge(three_city_nd, 'destination', bismarck)
+    g.add_edge(three_city_nd, 'distance', unknown_dist_3)
+    g.add_edge(arb_wi_city, 'city_state', wisco)
+
+    four_city_nd = g.add_node()
+    g.add_edge(four_city_nd, 'type', 'predicate')
+    arb_ca_city = g.add_node()
+    g.add_edge(arb_ca_city, 'name', 'Arbitrary California City')
+    g.add_edge(four_city_nd, 'source', arb_ca_city)
+    g.add_edge(four_city_nd, 'destination', bismarck)
+    unknown_dist_4 = g.add_node()
+    g.add_edge(four_city_nd, 'distance', unknown_dist_4)
+    g.add_edge(arb_ca_city, 'city_state', ca)
+    g.add_edge(unknown_dist_4, 'greater_than', unknown_dist_3)
+
+    san_fran = g.add_node()
+    g.add_edge(san_fran, 'name', 'San Francisco')
+    g.add_edge(san_fran, 'city_state', ca)
+    la = g.add_node()
+    g.add_edge(la, 'name', 'Los Angeles')
+    g.add_edge(la, 'city_state', ca)
+    geneva = g.add_node()
+    g.add_edge(geneva, 'name', 'Lake Geneva')
+    g.add_edge(geneva, 'city_state', wisco)
+    greenbay = g.add_node()
+    g.add_edge(greenbay, 'name', 'Greenbay')
+    g.add_edge(greenbay, 'city_state', wisco)
+    madison = g.add_node()
+    g.add_edge(madison, 'name', 'Madison')
+    g.add_edge(madison, 'city_state', wisco)
+
+    g.add_edge(bismarck, 'isa', 'city')
+    g.add_edge(san_fran, 'isa', 'city')
+    g.add_edge(la, 'isa', 'city')
+    g.add_edge(geneva, 'isa', 'city')
+    g.add_edge(greenbay, 'isa', 'city')
+    g.add_edge(madison, 'isa', 'city')
+
+    g.add_edge(wisco, 'isa', 'state')
+    g.add_edge(nd, 'isa', 'state')
+    g.add_edge(ca, 'isa', 'state')
+
+
     # print graph in graphviz format
     print(g.to_dot())
+    g.write_png('temp.png')
     # do a query ("name all animals that have fur")
-    qres = g.query('''
-        SELECT DISTINCT ?animal_name
+    # find all cities in California
+
+    qres = g.query(
+
+        '''
+        SELECT DISTINCT ?city_name1 ?city_name
         WHERE {
-            ?animal nxrdf:has ?fur ;
-                    nxrdf:name ?animal_name .
-            ?fur nxrdf:name "fur" .
+            ?city nxrdf:isa "city" ;
+                  nxrdf:city_state ?state ;
+                  nxrdf:name ?city_name .
+            ?state nxrdf:isa "state" ;
+                  nxrdf:name "California" .
+            ?state1 nxrdf:isa "state" ;
+                  nxrdf:name "Wisconsin" .
+            ?city1 nxrdf:isa "city" ;
+                  nxrdf:city_state ?state1 ;
+                  nxrdf:name ?city_name1 .
+
         }
-    ''')
+    '''
+    )
+
+    print("Results of the Query:")
     for row in qres:
         print(row)
 
