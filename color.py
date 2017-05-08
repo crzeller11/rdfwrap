d04baaddb9f129349601ee5a1cd7910125632401
Justin Li <justinnhli@gmail.com>
2017-05-08 10:55:18 -0700

    precalculate color distances

--- color.py
+++ color.py
@@ -1,3 +1,11 @@
+from ast import literal_eval
+from os.path import dirname, exists as file_exists, join as join_path, realpath
+
+DIRECTORY = dirname(realpath(__file__))
+
+COLOR_NAMES_FILE = join_path(DIRECTORY, 'color-centroids.tsv')
+COLOR_DISTANCES_FILE = join_path(DIRECTORY, 'color-distances')
+
 class Color:
     def __init__(self, r, g, b):
         self.r = r
@@ -18,3 +26,57 @@ class Color:
         if len(hexstr) == 7 and hexstr[0] == '#':
             hexstr = hexstr[1:]
         return Color(*(int(hexstr[i:i+2], 16) for i in range(0, 5, 2)))
+
+def create_distances():
+    colors = []
+    with open(COLOR_NAMES_FILE) as fd:
+        for line in fd:
+            line = line.strip()
+            if line[0] == '#':
+                continue
+            count, name, hexstr = line.split('\t')
+            color = Color.from_hex(hexstr)
+            others = []
+            for other, distances in colors:
+                others.append([other, color - other])
+            others = sorted(others, key=(lambda pair: pair[1]))
+            colors.append([color, others])
+    return colors
+
+def write_distances(colors):
+    str_colors = []
+    for color, distances in colors:
+        str_distances = []
+        for other, distance in distances:
+            str_distances.append([str(other), distance])
+        str_colors.append([str(color), str_distances])
+    with open(COLOR_DISTANCES_FILE, 'w') as fd:
+        fd.write(repr(str_colors))
+
+def read_distances():
+    with open(COLOR_DISTANCES_FILE) as fd:
+        colors = []
+        str_colors = literal_eval(fd.read())
+        for str_color, str_distances in str_colors:
+            color = Color.from_hex(str_color)
+            distances = []
+            for str_other, distance in str_distances:
+                other = Color.from_hex(str_other)
+                distances.append([other, distance])
+            colors.append([color, distances])
+        return colors
+
+def get_distances():
+    if file_exists(COLOR_DISTANCES_FILE):
+        return read_distances()
+    elif file_exists(COLOR_NAMES_FILE):
+        distances = create_distances()
+        write_distances(distances)
+        return distances
+    assert False, 'Cannot find color information'
+
+def main():
+    get_distances()
+
+if __name__ == '__main__':
+    main()
