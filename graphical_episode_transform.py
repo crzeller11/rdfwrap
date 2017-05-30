b96c5b88c7ae2db3ffb330011672e33ace2f38b3
Chloe <czeller@oxy.edu>
2017-05-29 20:51:24 -0700

    Added RGB values to the edges

--- graphical_episode_transform.py
+++ graphical_episode_transform.py
@@ -4,18 +4,18 @@ from chroma_wanderer import walk
 
 def graphical_episode_transform(): # takes in list of colors
     g = NXRDF()
-    colors = walk(10) # list of 10 colors
+    colors = walk(1) # list of 10 colors
 
     for time, color in enumerate(colors):
         node = g.add_node()
+        print(color)
         g.add_edge(node, 'time', time)
         g.add_edge(node, 'color_code', color)
-        g.add_edge(node, 'color_name', '') # isolate color name from color-centroids
+        # g.add_edge(node, 'color_name', '') # isolate color name from color-centroids
         g.add_edge(node, 'red', color.r) # how to get the RGB values of the color?
         g.add_edge(node, 'green', color.g)
         g.add_edge(node, 'blue', color.b)
-        g.add_edge(node, 'type', 'color')
-        g.add_edge(node, 'episode', '') # what is the difference between time and episode?
+        g.add_edge(node, 'type', 'episode')
 
     print(g.to_dot())
     g.write_png('temp.png')
