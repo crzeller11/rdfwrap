f39968b0e9da39868e386e2bdb4b3a12417c9ac8
Justin Li <justinnhli@gmail.com>
2017-05-30 16:18:59 -0700

    remove KNN code from chroma_wanderer

--- chroma_wanderer.py
+++ chroma_wanderer.py
@@ -1,30 +1,6 @@
-from collections import OrderedDict
 from random import randrange
 
 from color import Color
-
-class KNN:
-    def __init__(self, means=None, names=None):
-        if means is None:
-            means = []
-        if names is None:
-            names = []
-        self.means = dict(zip(means, names))
-    def classify(self, point):
-        return min(self.means.keys(), key=(lambda mean: abs(point - mean)))
-    @staticmethod
-    def from_file(file, limit=5):
-        colors = []
-        with open(file) as fd:
-            for line in fd:
-                line = line.strip()
-                if line[0] == '#':
-                    continue
-                name, hexstr = line.split('\t')
-                color = Color.from_hex(hexstr)
-                colors.append((color, name))
-        colors = list(reversed(colors))
-        return KNN(color for color, name in colors[:limit])
 
 def step(color, max_dist=8):
     rand = randrange(3)
