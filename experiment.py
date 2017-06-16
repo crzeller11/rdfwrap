60beeca016fad13cd7444fea3ee751f2f3c0bb35
Justin Li <justinnhli@gmail.com>
2017-06-16 16:28:22 -0700

    add flexible iteration to experiments

--- experiment.py
+++ experiment.py
@@ -21,12 +21,15 @@ class Experiment:
         self.order.extend(sorted(parameter_space.constants.keys()))
         self.order.extend(sorted(self.machine_info.keys()))
         self.order.extend(['_start_time', '_end_time'])
-    def run(self):
+    def _run(self, iterator_modifier=None):
         print_headers = True
         start_time = datetime.now().isoformat()
         output_file = '{}-{}-results.csv'.format(self.name, start_time)
         with open(output_file, 'a') as fd:
-            for parameters in self.parameter_space:
+            iterator = self.parameter_space
+            if iterator_modifier is not None:
+                iterator = iterator_modifier(iterator)
+            for parameters in iterator:
                 run_info = Namespace(
                     _start_time=datetime.now().isoformat(sep=' '),
                 )
@@ -41,3 +44,11 @@ class Experiment:
                 csv_row.update(**self.machine_info)
                 csv_row.update(**run_info)
                 fd.write(csv_row.to_csv_row(self.order) + '\n')
+    def run(self):
+        self._run()
+    def run_from(self, start=None):
+        self._run((lambda iterator: iterator.iter_from(start)))
+    def run_until(self, end=None):
+        self._run((lambda iterator: iterator.iter_until(end)))
+    def run_between(self, start=None, end=None):
+        self._run((lambda iterator: iterator.iter_between(start, end)))
