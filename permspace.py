b8e22cef010756702a847dd23c72cf37d1834b85
Justin Li <justinnhli@gmail.com>
2017-06-18 16:16:28 -0700

    update permspace

--- permspace.py
+++ permspace.py
@@ -2,38 +2,60 @@ from inspect import signature
 
 class Namespace:
     def __init__(self, **kwargs):
+        self._internal_ = {}
         self.update(**kwargs)
     def __eq__(self, other):
-        return isinstance(other, Namespace) and vars(self) == vars(other)
+        return isinstance(other, Namespace) and self._internal_ == other._internal_
     def __len__(self):
-        return len(self.__dict__)
+        return len(self._internal_)
     def __add__(self, other):
-        updated = self.__dict__
-        updated.update(other.__dict__)
+        updated = self._internal_
+        updated.update(other._internal_)
         return Namespace(**updated)
     def __contains__(self, key):
-        return key in self.__dict__
+        return key in self._internal_
     def __getitem__(self, key):
-        if key in self.__dict__:
-            return self.__dict__[key]
-        raise KeyError(key)
+        try:
+            return getattr(self, key)
+        except AttributeError as e:
+            raise KeyError(str(e))
     def __setitem__(self, key, value):
         setattr(self, key, value)
     def __delitem__(self, key):
-        if key in self.__dict__:
+        if key in self._internal_:
             delattr(self, key)
-        raise KeyError(key)
+        else:
+            raise KeyError(key)
+    def __getattr__(self, key):
+        if key in self.__dict__:
+            return self.__dict__[key]
+        if key in self._internal_:
+            return self._internal_[key]
+        raise AttributeError('{} object has no attribute {}'.format(repr(self.__class__.__name__), repr(key)))
+    def __setattr__(self, key, value):
+        self.__dict__[key] = value
+        if not (key.startswith('_') and key.endswith('_')):
+            self._internal_[key] = value
+    def __delattr__(self, key):
+        if key in self._internal_:
+            del self._internal_[key]
+            del self.__dict__[key]
     def __str__(self):
-        return 'Namespace(' + ', '.join('{}={}'.format(k, repr(v)) for k, v in sorted(self.__dict__.items())) + ')'
+        return 'Namespace(' + ', '.join('{}={}'.format(k, repr(v)) for k, v in sorted(self._internal_.items())) + ')'
     def update(self, **kwargs):
         for key, value in kwargs.items():
-            self[key] = value
+            try:
+                Namespace.__getattribute__(self, key)
+                raise KeyError('{} is reserved and is not allowed as a key'.format(repr(key)))
+            except AttributeError:
+                self._internal_[key] = value
+                self.__dict__[key] = value
     def keys(self):
-        return self.__dict__.keys()
+        return self._internal_.keys()
     def values(self):
-        return self.__dict__.values()
+        return self._internal_.values()
     def items(self):
-        return self.__dict__.items()
+        return self._internal_.items()
     def _expand_order_(self, order):
         order = list(order)
         return order + sorted(set(self.keys()) - set(order))
@@ -44,6 +66,35 @@ class Namespace:
         order = self._expand_order_(order)
         return '\t'.join(str(self[k]) for k in order)
 
+class MixedRadix:
+    def __init__(self, radixes, init_values=None):
+        self.radixes = radixes
+        if init_values is None:
+            self._state_ = len(radixes) * [0]
+        else:
+            assert len(radixes) == len(init_values)
+            assert all(place < cap for place, cap in zip(init_values, radixes))
+            self._state_ = list(init_values)
+        self._state_[-1] -= 1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        return self.next()
+    def next(self, min_place=None):
+        if min_place is None:
+            min_place = len(self._state_) - 1
+        for index in range(min_place + 1, len(self._state_)):
+            self._state_[index] = 0
+        for index in reversed(range(min_place + 1)):
+            if self._state_[index] < self.radixes[index] - 1:
+                self._state_[index] += 1
+                break
+            elif index == 0:
+                raise StopIteration
+            else:
+                self._state_[index] = 0
+        return self._state_
+
 class ParameterSpaceIterator:
     def __init__(self, pspace, start=None, end=None):
         if start is None:
@@ -51,63 +102,35 @@ class ParameterSpaceIterator:
         if end is None:
             end = {}
         self.pspace = pspace
-        self._state = len(self.pspace.order) * [0]
+        start_indices = len(self.pspace.order) * [0]
         for key, value in start.items():
             assert key in self.pspace.independents, 'unknown start parameter: {}'.format(key)
             assert value in self.pspace.independents[key], 'unknown value for start parameter {}: {}'.format(key, repr(value))
             index = self.pspace.order.index(key)
-            self._state[index] = self.pspace.independents[key].index(value)
-        self._state[-1] -= 1
-        self._end_state = None
+            start_indices[index] = self.pspace.independents[key].index(value)
+        self._state_ = MixedRadix(self.pspace.ordered_sizes, start_indices)
+        self._end_indices_ = None
         if end:
-            self._end_state = len(self.pspace.order) * [0]
+            self._end_indices_ = len(self.pspace.order) * [0]
             for key, value in end.items():
                 assert key in self.pspace.independents, 'unknown end parameter: {}'.format(key)
                 assert value in self.pspace.independents[key], 'unknown value for end parameter {}: {}'.format(key, repr(value))
                 index = self.pspace.order.index(key)
-                self._end_state[index] = self.pspace.independents[key].index(value)
+                self._end_indices_[index] = self.pspace.independents[key].index(value)
     def __iter__(self):
         return self
     def __next__(self):
-        self._update_state_()
-        result = self._expand_values_()
-        conflicts = self._check_filters_(result)
+        conflicts = True
+        min_place = len(self.pspace.order) - 1
         while conflicts:
-            largest_index = min(max(self.pspace.order.index(parameter) for parameter in parameters) for parameters in conflicts)
-            self._update_state_(largest_index=largest_index)
-            result = self._expand_values_()
-            conflicts = self._check_filters_(result)
-        return result
-    def _update_state_(self, largest_index=None):
-        if largest_index is None:
-            largest_index = len(self._state) - 1
-        for index in range(largest_index + 1, len(self._state)):
-            self._state[index] = 0
-        parameters = self.pspace.order[:largest_index + 1]
-        for index, parameter in reversed(tuple(enumerate(parameters))):
-            if self._state[index] < len(self.pspace.independents[parameter]) - 1:
-                self._state[index] += 1
-                if self._end_state and self._state >= self._end_state:
-                    raise StopIteration
-                break
-            elif index == 0:
+            next_index = self._state_.next(min_place)
+            if self._end_indices_ and next_index >= self._end_indices_:
                 raise StopIteration
-            else:
-                self._state[index] = 0
-    def _expand_values_(self):
-        result = Namespace()
-        for parameter, index in zip(self.pspace.order, self._state):
-            value = self.pspace.independents[parameter][index]
-            result[parameter] = value
-        for parameter in self.pspace.independents.keys():
-            if parameter not in result:
-                result[parameter] = self.pspace.independents[parameter][0]
-        for parameter, value in self.pspace.constants.items():
-            result[parameter] = value
-        for parameter in self.pspace.dependents_topo:
-            fn = self.pspace.dependents[parameter]
-            result[parameter] = fn(**result)
-        return result
+            next_state = self.pspace._get_namespace_from_indices_(next_index)
+            conflicts = self._check_filters_(next_state)
+            if conflicts:
+                min_place = min(max(self.pspace.order.index(parameter) for parameter in parameters) for parameters in conflicts)
+        return next_state
     def _check_filters_(self, result):
         conflicts = []
         for fn in self.pspace.filters:
@@ -197,6 +220,9 @@ class PermutationSpace:
         for values in self.independents.values():
             product *= len(values)
         return product
+    @property
+    def ordered_sizes(self):
+        return [len(self.independents[parameter]) for parameter in self.order]
     def __len__(self):
         return len(list(self.__iter__()))
     def __iter__(self):
@@ -207,8 +233,31 @@ class PermutationSpace:
         return ParameterSpaceIterator(self, end=end)
     def iter_between(self, start=None, end=None):
         return ParameterSpaceIterator(self, start=start, end=end)
-    def add_filter(self, fn):
-        wrapped_function = FunctionWrapper(fn)
+    def iter_only(self, key, value):
+        start_index = len(self.order) * [0]
+        key_index = self.order.index(key)
+        value_index = self.independents[key].index(value)
+        start_index[key_index] = value_index
+        end_index = MixedRadix(self.ordered_sizes, start_index).next(key_index)
+        start = self._get_independents_from_indices_(start_index)
+        end = self._get_independents_from_indices_(end_index)
+        return ParameterSpaceIterator(self, start=start, end=end)
+    def add_filter(self, filter_fn):
+        wrapped_function = FunctionWrapper(filter_fn)
         if not set(wrapped_function.arguments) <= self.parameters:
             raise ValueError('filter contains undefined/unreachable arguments')
         self.filters.append(wrapped_function)
+    def _get_independents_from_indices_(self, indices):
+        assert len(indices) == len(self.order)
+        assert all(index < len(self.independents[key]) for index, key in zip(indices, self.order))
+        result = Namespace()
+        for parameter, index in zip(self.order, indices):
+            result[parameter] = self.independents[parameter][index]
+        return result
+    def _get_namespace_from_indices_(self, indices):
+        result = self._get_independents_from_indices_(indices)
+        for parameter in self.dependents_topo:
+            result[parameter] = self.dependents[parameter](**result)
+        for parameter, value in self.constants.items():
+            result[parameter] = value
+        return result
